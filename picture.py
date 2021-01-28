5e096b15e402e988e083deeeb301935e78bf2b03
wsc <59681039+mnwscisme@users.noreply.github.com>
2021-01-28 11:09:51 +0800

    Add files via upload

--- picture.py
+++ picture.py
@@ -4,55 +4,41 @@ from PIL import Image
 import numpy
 import cv2
 
-
-def main():
-	# 打开设备
-	# 枚举设备
-	device_manager = gx.DeviceManager() 
-	dev_num, dev_info_list = device_manager.update_device_list()
-	if dev_num == 0:
-		sys.exit(1)
-	# 获取设备基本信息列表
-	str_sn = dev_info_list[0].get("sn")
-	# 通过序列号打开设备
-	cam = device_manager.open_device_by_sn(str_sn)
-	# 导入配置信息
-	# cam.import_config_file("./import_config_file.txt")
-	# 开始采集
-	cam.stream_on()
-	
-	# 帧率
-	fps = cam.AcquisitionFrameRate.get()  
-	# 视频的宽高
-	size = (int(cam.Width.get()),int(cam.Height.get()))
-
-	fourcc = cv2.VideoWriter_fourcc(*'XVID')
-	out = cv2.VideoWriter('2.avi',fourcc,fps,size)
-	#cv2.namedWindow('origin', flags = cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
-
-	while (1):
-    cap = cam.data_stream[0].get_image()
-		cap = cap.convert("RGB")
-		frame = cap.get_numpy_array()
-		frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-		
-		if cap is None:
-			continue
-		#frame = cv2.flip(frame,1)		
-		out.write(frame)
-		cv2.imshow('origin', frame)
-
-		if cv2.waitKey(1) == ord('q'):
-			break
-	cv2.destroyWindow('origin')
-	
-	# 停止采集
-	cam.stream_off()
-        #释放
-	out.release()
-	# close device
-	cam.close_device()
-	cv2.destroyAllWindows()  
-
-if __name__ == "__main__":
-    main()
+# 枚举设备。dev_info_list 是设备信息列表,列表的元素个数为枚举到的设备个数,列表元素是字典,其中包含设备索引(index)、ip 信息(ip)等设备信息
+device_manager = gx.DeviceManager()
+dev_num, dev_info_list = device_manager.update_device_list()
+if dev_num == 0:
+	sys.exit(1)
+# 打开设备
+# 获取设备基本信息列表
+strSN = dev_info_list[0].get("sn")
+# 通过序列号打开设备
+cam = device_manager.open_device_by_sn(strSN)
+# 开始采集
+cam.stream_on()
+# 获取流通道个数
+# 如果 int_channel_num == 1,设备只有一个流通道,列表 data_stream 元素个数为 1
+# 如果 int_channel_num > 1,设备有多个流通道,列表 data_stream 元素个数大于 1
+# 目前千兆网相机、USB3.0、USB2.0 相机均不支持多流通道。
+# int_channel_num = cam.get_stream_channel_num()
+# 获取数据
+# num 为采集图片次数
+num = 10
+for i in range(num):
+	# 从第 0 个流通道获取一幅图像
+	raw_image = cam.data_stream[0].get_image()
+	# 从彩色原始图像获取 RGB 图像
+	rgb_image = raw_image.convert("RGB")
+	if rgb_image is None:
+		continue
+	# 从 RGB 图像数据创建 numpy 数组
+	numpy_image = rgb_image.get_numpy_array()
+	if numpy_image is None:
+		continue
+	# 显示并保存获得的 RGB 图片
+	image = Image.fromarray(numpy_image, 'RGB')
+	image.show()
+	image.save(str(i)+"image.jpg")
+# 停止采集,关闭设备
+cam.stream_off()
+cam.close_device()
