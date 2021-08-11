a6cf30b8d4900348f2aa6ec7bed2067c91ea762c
wsc <59681039+mnwscisme@users.noreply.github.com>
2021-08-11 11:01:17 +0800

    Update video.py

--- video.py
+++ video.py
@@ -20,8 +20,7 @@ while(True):
     # 转变成Image
     frame = Image.fromarray(np.uint8(frame))
 
-    # 进行检测
-    frame = np.array(yolo.detect_image(frame))
+
 
     # RGBtoBGR满足opencv显示格式
     frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
