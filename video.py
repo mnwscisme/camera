94ee66bb5496a209d1d791de66e7af88dae996a7
wsc <59681039+mnwscisme@users.noreply.github.com>
2021-08-11 11:00:53 +0800

    Update video.py

--- video.py
+++ video.py
@@ -1,17 +1,13 @@
 #-------------------------------------#
-#       调用摄像头检测
+#       调用摄像头
 #-------------------------------------#
-from yolo import YOLO
 from PIL import Image
 import numpy as np
 import cv2
 import time
-import tensorflow as tf
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-yolo = YOLO()
+
 # 调用摄像头
 capture=cv2.VideoCapture(0) # capture=cv2.VideoCapture("1.mp4")
 fps = 0.0
